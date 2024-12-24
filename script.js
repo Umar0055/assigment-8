856f9e213ac4184ee747bcc5b43030eb20298872
umar0055 <umarsohail648@gmail.com>
2024-12-24 15:39:56 +0500

    assigment no8

---
+++ script.js
@@ -0,0 +1,158 @@
+// assigment no 1
+// var inputuser1=prompt("first number");
+// var inputuser2=prompt("second number");
+// if(inputuser1 > inputuser2){
+//     console.log("user1 win")
+// }else if(inputuser2> inputuser1){
+//     console.log("user2 win")
+// }else{
+//     console.log("invilade option")
+// }
+
+//assigment no 2
+// var inputuser1=prompt("first number");
+// var inputuser2=prompt("second number");
+// var inputuser3=prompt("third number");
+// if(inputuser1 > inputuser2 && inputuser1>inputuser3){
+//     console.log("user1 win")
+// }else if(inputuser2> inputuser1 && inputuser2> inputuser3){
+//     console.log("user2 win")
+// }else if(inputuser3> inputuser1 && inputuser3> inputuser2){
+//     console.log("user3 win")
+// }
+// else{
+//     console.log("invilade option")
+// }
+
+//assigment no 3
+// var inputuser=prompt("Enter number");
+// if(inputuser <= -1){
+//     console.log("number is  negative")
+// }else if(inputuser == 0){
+//     console.log("number is zero")
+// }else{
+//     console.log("number is positive")
+// }
+
+// assigment no 4
+// var inputuser=prompt("enter number");
+// if(inputuser % 5 == 0 || inputuser %11 ==0){
+// console.log("number is divisible")
+// }else{
+//     console.log("numver is not divisible")
+// }
+
+// assigemnt no 5
+// var inputuser=prompt("enter number");
+// if(inputuser % 2 == 0 ){
+// console.log("number is even")
+// }else{
+//     console.log("numver is odd")
+// }
+
+// assigment no 8 working
+// var inputuser=prompt("enter ");
+// if(inputuser >='a'&& inputuser<='e' ){
+//     console.log("vowels")
+// }else{
+//     console.log("constant")
+// }
+
+
+//assigment no 9
+// var inputuser=prompt("enter ");
+// if(inputuser >= "a" && inputuser <= "z"){
+//     console.log("abc")
+// }else if (inputuser >= '1'&&inputuser<='1000000000'){
+//     console.log("number")
+// }
+// else{
+//     console.log(" special character")
+// }
+
+
+// assigemnt no 10
+// var inputuser=prompt("enter ");
+// if(inputuser >= "A" && inputuser <= "Z"){
+//     console.log("uppercase")
+// }else if (inputuser >= "a"&&inputuser<="z"){
+//     console.log("lowecase")
+// }
+// else{
+//     console.log("invilade option")
+//}
+
+// assigment no 11
+// var inputuser=prompt("enter a number ");
+// var monday =1;
+// var tuesday =2;
+// var wednesday =3;
+// var thusday =4;
+// var friday =5;
+// var satuday =6;
+// var sunday =7;
+// if(inputuser== monday){
+//     console.log("monday")
+// }else if(inputuser == tuesday){
+//     console.log("tuesday")
+// }else if(inputuser == wednesday){
+//     console.log(" wednesday")
+// }else if(inputuser == thusday){
+//     console.log("thusday")
+// }else if(inputuser == friday){
+//     console.log("friday")
+// }else if(inputuser == satuday){
+//     console.log("satuday")
+// }else if(inputuser == sunday){
+//     console.log("sunday")
+// }else{
+//     console.log("invilade")
+// }
+
+// assigment no 12
+// var inputuser = prompt("enter month ");
+// var jan ="january";
+// var feb ="febtuary";
+// var  march="march";
+// var april ="april";
+// var may ="may";
+// var june ="june";
+// var july ="july";
+// var aug ="august";
+// var sep ="september";
+// var oct ="october";
+// var nov ="november";
+// var dec= "december";
+// if (inputuser === "jan" || inputuser === "march" || inputuser === "may" || inputuser === "july" || inputuser === "aug" || inputuser === "oct" || inputuser === "dec") {
+//     console.log("31 days");
+// } else if (inputuser === "april" || inputuser === "june" || inputuser === "sep" || inputuser === "nov") {
+//     console.log("30 days");
+// } else if (inputuser === "feb") {
+//     console.log("28 or 29 days");
+// } else {
+//     console.log("Invalid ");
+// }
+
+// assigment no 19
+// var userInput = prompt("enter marks");
+// if(userInput >= 90 && userInput <= 100){
+//     console.log("grade a");
+// }
+// else if(userInput >= 80 && userInput < 89){
+//     console.log("grade b");
+// }
+// else if(userInput >= 70  && userInput < 79){
+//     console.log("grade c");
+// }
+// else if(userInput >= 60 && userInput < 69){
+//     console.log("grade d");
+// }
+// else if(userInput >= 40  && userInput < 59){
+//     console.log("grade e");
+// }
+// else if(userInput >= 0 && userInput < 39){
+//     console.log("grade f");
+// }
+// else{
+//     console.log("Invalid ");
+// }
